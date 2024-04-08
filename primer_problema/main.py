3b222f630c1e55b102f0f9ef0483208070fd5329
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2024-04-07 23:09:43 -0300

    Agregado snippet que puede ser util para la solucion.

--- primer_problema/main.py
+++ primer_problema/main.py
@@ -1,5 +1,11 @@
 def main() -> None:
-    print("Hola Mundo!")
+    incompatibilidades = set([])
+    incompatibilidades.add(frozenset([1,10]))
+    incompatibilidades.add(frozenset([1,2]))
+    incompatibilidades.add(frozenset([2,1]))
+    incompatibilidades.add(frozenset([1,10]))
+    incompatibilidades.add(frozenset([10,1]))
+    print(incompatibilidades)
 
 if __name__ == "__main__":
     main()

49e2ca8bc4723513389e2c617c4631ea57c0bdef
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2024-04-07 23:46:23 -0300

    Agregada lectura basica del archivo del problema.

--- primer_problema/main.py
+++ primer_problema/main.py
@@ -1,11 +1,31 @@
+ARCHIVO_PROBLEMA = 'primer_problema.txt'
+
+COMENTARIO = 'c'
+PROBLEMA = 'p'
+INCOMPATIBILIDAD = 'e'
+TIEMPO_LAVADO = 'n'
+
 def main() -> None:
-    incompatibilidades = set([])
-    incompatibilidades.add(frozenset([1,10]))
-    incompatibilidades.add(frozenset([1,2]))
-    incompatibilidades.add(frozenset([2,1]))
-    incompatibilidades.add(frozenset([1,10]))
-    incompatibilidades.add(frozenset([10,1]))
-    print(incompatibilidades)
+    # incompatibilidades = set([])
+    # incompatibilidades.add(frozenset([1,10]))
+    # incompatibilidades.add(frozenset([1,2]))
+    # incompatibilidades.add(frozenset([2,1]))
+    # incompatibilidades.add(frozenset([1,10]))
+    # incompatibilidades.add(frozenset([10,1]))
+    # print(incompatibilidades)
+    
+    with open(ARCHIVO_PROBLEMA) as archivo:
+        for linea in archivo:
+            palabras = linea.split()
+            match palabras[0]:
+                case COMENTARIO:
+                    print('comentario')
+                case PROBLEMA:
+                    print('definicion del problema')
+                case INCOMPATIBILIDAD:
+                    print('incompatibilidad')
+                case TIEMPO_LAVADO:
+                    print('tiempo de lavado')
 
 if __name__ == "__main__":
     main()

b984684c647aea91176da028c789328db95808db
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2024-04-10 10:20:57 -0300

    Extraida de main la logica de lectura del archivo del problema.

--- primer_problema/main.py
+++ primer_problema/main.py
@@ -1,31 +1,35 @@
-ARCHIVO_PROBLEMA = 'primer_problema.txt'
+class TipoDeLinea():
+  COMENTARIO = 'c'
+  PROBLEMA = 'p'
+  INCOMPATIBILIDAD = 'e'
+  TIEMPO_LAVADO = 'n'
 
-COMENTARIO = 'c'
-PROBLEMA = 'p'
-INCOMPATIBILIDAD = 'e'
-TIEMPO_LAVADO = 'n'
+def cargar_problema_desde_archivo():
+  ARCHIVO_PROBLEMA = 'primer_problema.txt'
+  
+  with open(ARCHIVO_PROBLEMA) as archivo:
+    for linea in archivo:
+      palabras = linea.split()
+      match palabras[0]:
+        case TipoDeLinea.COMENTARIO:
+          print('comentario')
+        case TipoDeLinea.PROBLEMA:
+          print('definicion del problema')
+        case TipoDeLinea.INCOMPATIBILIDAD:
+          print('incompatibilidad')
+        case TipoDeLinea.TIEMPO_LAVADO:
+          print('tiempo de lavado')
+
 
 def main() -> None:
-    # incompatibilidades = set([])
-    # incompatibilidades.add(frozenset([1,10]))
-    # incompatibilidades.add(frozenset([1,2]))
-    # incompatibilidades.add(frozenset([2,1]))
-    # incompatibilidades.add(frozenset([1,10]))
-    # incompatibilidades.add(frozenset([10,1]))
-    # print(incompatibilidades)
-    
-    with open(ARCHIVO_PROBLEMA) as archivo:
-        for linea in archivo:
-            palabras = linea.split()
-            match palabras[0]:
-                case COMENTARIO:
-                    print('comentario')
-                case PROBLEMA:
-                    print('definicion del problema')
-                case INCOMPATIBILIDAD:
-                    print('incompatibilidad')
-                case TIEMPO_LAVADO:
-                    print('tiempo de lavado')
+  # incompatibilidades = set([])
+  # incompatibilidades.add(frozenset([1,10]))
+  # incompatibilidades.add(frozenset([1,2]))
+  # incompatibilidades.add(frozenset([2,1]))
+  # incompatibilidades.add(frozenset([1,10]))
+  # incompatibilidades.add(frozenset([10,1]))
+  # print(incompatibilidades)
+  cargar_problema_desde_archivo()
 
 if __name__ == "__main__":
-    main()
+  main()

dd56e250a41030384eeb330dcd83694eaee54068
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2024-04-12 11:46:18 -0300

    Algoritmo preliminar para encontrar los lavados compatibles que minimicen el tiempo total de lavado.

--- primer_problema/main.py
+++ primer_problema/main.py
@@ -1,50 +1,105 @@
 import pprint
 
-class TipoDeLinea():
-  COMENTARIO = 'c'
-  PROBLEMA = 'p'
-  INCOMPATIBILIDAD = 'e'
-  TIEMPO_LAVADO = 'n'
+
+class TipoDeLinea:
+    COMENTARIO = "c"
+    PROBLEMA = "p"
+    INCOMPATIBILIDAD = "e"
+    TIEMPO_LAVADO = "n"
+
 
 def cargar_parametros_del_problema(archivo):
-  linea = archivo.readline().split()
-    
-  while linea[0] != TipoDeLinea.PROBLEMA:
     linea = archivo.readline().split()
 
-  return int(linea[2]), int(linea[3])
+    while linea[0] != TipoDeLinea.PROBLEMA:
+        linea = archivo.readline().split()
+
+    return int(linea[2]), int(linea[3])
+
 
 def cargar_problema_desde_archivo():
-  ARCHIVO_PROBLEMA = 'primer_problema.txt'
-  
-  with open(ARCHIVO_PROBLEMA) as archivo:
-    
-    cantidad_prendas, cantidad_incompatibilidades = cargar_parametros_del_problema(archivo)
+    ARCHIVO_PROBLEMA = "primer_problema.txt"
 
-    prendas = []
-    incompatibilidades = {k: [] for k in range(1, cantidad_prendas+1)}
+    with open(ARCHIVO_PROBLEMA) as archivo:
 
-    for linea in archivo:
-      palabras = linea.split()
-      tipo = palabras[0]
-      
-      match tipo:
-        case TipoDeLinea.COMENTARIO:
-          print('comentario')
-        case TipoDeLinea.PROBLEMA:
-          print('definicion del problema')
-        case TipoDeLinea.INCOMPATIBILIDAD:
-          incompatibilidades[int(palabras[1])].append(int(palabras[2]))
-        case TipoDeLinea.TIEMPO_LAVADO:
-          prendas.append((int(palabras[1]), int(palabras[2])))
-    
-    prendas.sort(reverse=True, key=lambda e: e[1])
-    return prendas, incompatibilidades
+        cantidad_prendas, cantidad_incompatibilidades = cargar_parametros_del_problema(
+            archivo
+        )
+
+        prendas = {
+            numero_prenda: {"tiempo_lavado": 0, "incompatible_con": []}
+            for numero_prenda in range(1, cantidad_prendas + 1)
+        }
+
+        for linea in archivo:
+            palabras = linea.split()
+            tipo = palabras[0]
+
+            match tipo:
+                case TipoDeLinea.INCOMPATIBILIDAD:
+                    numero_prenda = int(palabras[1])
+                    numero_prenda_incompatible = int(palabras[2])
+                    prendas[numero_prenda]["incompatible_con"].append(
+                        numero_prenda_incompatible
+                    )
+                case TipoDeLinea.TIEMPO_LAVADO:
+                    numero_prenda = int(palabras[1])
+                    tiempo_lavado = int(palabras[2])
+                    prendas[numero_prenda]["tiempo_lavado"] = tiempo_lavado
+
+        return prendas
+
 
 def main() -> None:
-  prendas, incompatibilidades = cargar_problema_desde_archivo()
-  # pprint.pprint(prendas)
-  pprint.pprint(incompatibilidades)
+    prendas = cargar_problema_desde_archivo()
+
+    prendas_ordenadas = list(
+        map(
+            lambda item: item[0],
+            sorted(
+                prendas.items(),
+                key=lambda item: item[1]["tiempo_lavado"],
+            ),
+        )
+    )
+
+    lavados = [[] for _ in range(len(prendas_ordenadas))]
+
+    while prendas_ordenadas:
+        prenda_actual = prendas_ordenadas.pop()
+        asignada_a_lavado = False
+
+        i = 0
+        while not asignada_a_lavado and i < len(lavados):
+            lavado_actual = lavados[i]
+
+            if set(prendas[prenda_actual]["incompatible_con"]).isdisjoint(
+                lavado_actual
+            ):
+                lavado_actual.append(prenda_actual)
+                asignada_a_lavado = True
+            i += 1
+
+    lavados = list(
+        map(
+            lambda lavado: [
+                (prenda, prendas[prenda]["tiempo_lavado"]) for prenda in lavado
+            ],
+            filter(lambda lavado: lavado, lavados),
+        )
+    )
+    pprint.pprint(lavados)
+    lavados = list(
+        map(
+            lambda lavado: max(
+                tiempo_lavado for numero_prenda, tiempo_lavado in lavado
+            ),
+            lavados,
+        )
+    )
+    pprint.pprint(lavados)
+    print(sum(lavados))
+
 
 if __name__ == "__main__":
-  main()
+    main()
